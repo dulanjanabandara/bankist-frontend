5a40853ada0002182bb2760246db465be8e250f8
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-06-16 19:58:34 +0530

    smooth scrolling for nav bar links were added.

--- script.js
+++ script.js
@@ -66,6 +66,30 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 ////////////////////////////////////////////////////////////////
+// Page navigation
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
 // console.log(document.documentElement);
