1153857a1ba89ed20a2a6e27e823a881ebf86d1d
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-06-17 00:07:37 +0530

    added functionality to tabbed component section and navigation bar opacity changing effect.

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+const nav = document.querySelector('.nav');
 
 ///////////////////////////////////////
 // Modal window
@@ -35,7 +39,6 @@ document.addEventListener('keydown', function (e) {
 
 ////////////////////////////////////////////////////////////////
 // Button scrolling
-
 btnScrollTo.addEventListener('click', function (e) {
   // e.preventDefault();
   const s1coords = section1.getBoundingClientRect();
@@ -67,7 +70,6 @@ btnScrollTo.addEventListener('click', function (e) {
 
 ////////////////////////////////////////////////////////////////
 // Page navigation
-
 // document.querySelectorAll('.nav__link').forEach(function (el) {
 //   el.addEventListener('click', function (e) {
 //     e.preventDefault();
@@ -91,11 +93,6 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
 
 ////////////////////////////////////////////////////////////////
 // Tabbed components
-
-const tabs = document.querySelectorAll('.operations__tab');
-const tabsContainer = document.querySelector('.operations__tab-container');
-const tabsContent = document.querySelectorAll('.operations__content');
-
 tabsContainer.addEventListener('click', function (e) {
   const clicked = e.target.closest('.operations__tab');
 
@@ -111,6 +108,49 @@ tabsContainer.addEventListener('click', function (e) {
     .querySelector(`.operations__content--${clicked.dataset.tab}`)
     .classList.add('operations__content--active');
 });
+
+////////////////////////////////////////////////////////////////
+// Menu fade animation
+// const handleHover = function (e, opacity) {
+//   if (e.target.classList.contains('nav__link')) {
+//     const link = e.target;
+//     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+//     const logo = link.closest('.nav').querySelector('img');
+
+//     siblings.forEach(el => {
+//       if (el !== link) el.style.opacity = opacity;
+//     });
+
+//     logo.style.opacity = opacity;
+//   }
+// };
+
+// nav.addEventListener('mouseover', function (e) {
+//   handleHover(e, 0.5);
+// });
+
+// nav.addEventListener('mouseout', function (e) {
+//   handleHover(e, 1);
+// });
+
+const handleHover = function (e) {
+  console.log(this);
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = this;
+    });
+
+    logo.style.opacity = this;
+  }
+};
+
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+
+nav.addEventListener('mouseout', handleHover.bind(1));
 
 ////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////
